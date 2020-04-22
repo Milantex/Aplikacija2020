0f10455195b17c8e3b4ee579301aac4a5a970f5c
Milan Tair <milan.tair@gmail.com>
2020-04-22 20:50:35 +0200

    Implement the use of JWT

---
+++ config/jwt.secret.ts
@@ -0,0 +1,5 @@
+export const jwtSecret: string = '87ry49ghboundfv948hgv984hvrnewjv594vh0i5v4n' +
+                                 '89rt0hidfnvg08hgubivdrfev97134g9fubvfreibv9' +
+                                 '-9834uhivnibhv8gv34hioqrne 8954y4q39t u8048' +
+                                 '9-7879tgfhbnj80t7gyvhibn0[8y954564t1rgegdf]' +
+                                 '0584yhuonvgfre8gh34hbg8tbhfvnpq98 g34q348yu';
